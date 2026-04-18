719c4952c63b16f743464a37fe45e8cba50b2dbb
Thea Guevarra <rainepguevarra@gmail.com>
2026-04-18 19:12:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Web Development 111 Project - Static Website
+
+GUEVARRA, Thea
+CHIEFE, John Willy
+DE JUAN, Jamie Eliz
+MURRILLO, Kristine Mae
+
+BSIT 1-Y2-4
+
+/project desc.
+
+/features implemented.
+
+/screenshots.
---
+++ js/script.js
@@ -0,0 +1,48 @@
+const lightbox = document.createElement("div")
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.gallery-item img')
+
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.dataset.full
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
+
+const word = document.querySelector('.hero h1 .cycle-word')
+const fonts = [
+    'Fascinate',
+    'Permanent Marker', 
+    'Rubik Marker Hatch',
+    'Special Elite',
+    'Abril Fatface',
+    'Special Gothic Expanded One'
+]
+
+let i = 0
+const interval = setInterval(() => {
+    word.style.fontFamily = `'${fonts[i]}', sans-serif`
+    i++
+    if (i >= fonts.length) {
+        clearInterval(interval)
+    }
+}, 350)
+
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('nav ul')
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('open')
+})
